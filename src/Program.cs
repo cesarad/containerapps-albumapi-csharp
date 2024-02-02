cf5db500081a7043539388ca769846f9140fc880
César Álvarez Doval <cesarad@hotmail.com>
2024-02-02 10:15:32 +0100

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -48,6 +48,7 @@ record Album(int Id, string Title, string Artist, double Price, string Image_url
             new Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
             new Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
             new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
+            new Album(6, "How Dapr your Love", "Gee Bees", 11.99, "https://aka.ms/albums-containerappslogo")
          };
 
         return albums; 
